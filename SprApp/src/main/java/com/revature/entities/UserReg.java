b604ba1eaad3bd054f0230e888ca705010ec0d7c
randythiencao <randythiencao@yahoo.com>
2017-12-06 23:31:49 -0500

    added simple login/reg check

---
+++ SprApp/src/main/java/com/revature/entities/UserReg.java
@@ -0,0 +1,59 @@
+package com.revature.entities;
+
+public class UserReg {
+	private int id;
+    private String email;
+    private String password;
+    private String firstname;
+    private String lastname;
+	public UserReg(int id, String email, String password, String firstname, String lastname) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.password = password;
+		this.firstname = firstname;
+		this.lastname = lastname;
+	}
+	public UserReg() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	@Override
+	public String toString() {
+		return "UserReg [id=" + id + ", email=" + email + ", password=" + password + ", firstname=" + firstname
+				+ ", lastname=" + lastname + "]";
+	}
+    
+    
+      
+}
