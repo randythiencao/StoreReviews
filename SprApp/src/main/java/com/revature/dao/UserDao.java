3c128a10b90067454fb5135a63811ab8a224ca1b
randythiencao <randythiencao@yahoo.com>
2017-12-07 15:53:45 -0500

    merge

---
+++ SprApp/src/main/java/com/revature/dao/UserDao.java
@@ -0,0 +1,15 @@
+package com.revature.dao;
+
+import java.util.List;
+
+import com.revature.entities.Restaurant;
+import com.revature.entities.Review;
+import com.revature.entities.User;
+
+public interface UserDao {
+	List<User> getAllUsers();
+	User getUserById(int userId);
+	User addUser(User u);
+	User addUserReview (User user, Restaurant restaurant, Review review);
+	User findByUsername(String Username);
+}
